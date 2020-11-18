9c9461ff0e5a36870d6d4e2eac27d97052cc76e6
Dr. CrownPointTMD <69466128+CrownPointTMD@users.noreply.github.com>
2020-11-18 17:23:41 +0000

    Create softshutdown.lua

---
+++ softshutdown.lua
@@ -0,0 +1,53 @@
+local TeleportService = game:GetService("TeleportService")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+ 
+------------------------------------------------------------
+
+local MessageToClone = script:FindFirstChild("Message")
+ 
+local function newMessage(player, message)
+    local tempMessage = MessageToClone:Clone()
+    tempMessage.Parent = player:WaitForChild("PlayerGui")
+    tempMessage:FindFirstChild("Frame"):FindFirstChild("TextLabel").Text = message
+end
+
+------------------------------------------------------------
+if game.PrivateServerId ~= "" and game.PrivateServerOwnerId == 0 then
+    local waitTime = 5
+   
+    Players.PlayerAdded:Connect(function(player)
+        newMessage(player, "")
+       	wait(waitTime)
+        waitTime = waitTime / 15
+        TeleportService:Teleport(game.PlaceId, player)
+    end)
+   
+    for i, player in pairs(Players:GetPlayers()) do
+        newMessage(player, "We're currently restarting our servers, The Wherry Lines will restart and teleport you to the main game when the update has finished.")
+       
+        TeleportService:Teleport(game.PlaceId, player)
+        wait(waitTime)
+        waitTime = waitTime / 15
+	end
+------------------------------------------------------------
+elseif RunService:IsStudio() == false then
+    game:BindToClose(function()
+        spawn(function()
+            local reservedServerCode = TeleportService:ReserveServer(game.PlaceId)
+            for _, player in pairs(Players:GetPlayers()) do
+                newMessage(player, "We're restarting our servers for a update, The Wherry Lines will be back online shortly.")
+               
+                TeleportService:TeleportToPrivateServer(game.PlaceId, reservedServerCode, {player})
+            end
+            Players.PlayerAdded:Connect(function(player)
+                newMessage(player, "We're restarting our servers for an update, The Wherry Lines will back online shortly.")
+               
+                TeleportService:TeleportToPrivateServer(game.PlaceId, reservedServerCode, {player})
+            end)
+        end)
+        while #Players:GetPlayers() > 0 do
+            wait(1)
+        end
+    end)   
+end
